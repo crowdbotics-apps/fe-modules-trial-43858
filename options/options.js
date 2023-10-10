adf3befd0de8bd6d80160ef47abba00439ffd1d0
Muhammad Shoaib <muhammad.shoaib@crowdbotics.com>
2023-10-10 22:34:57 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "fe-modules-trial-43858",
   url: "https://fe-modules-trial-43858.botics.co",
@@ -7,9 +5,9 @@ export const globalOptions = {
   screenOptions: {
     headerShown: false
   }
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
