95867b5f7923a82bf2ca8d9924aef02f813e9051
Muhammad Shoaib <muhammad.shoaib@crowdbotics.com>
2023-10-10 22:42:01 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "fe-modules-trial-43858",
-  url: "https://fe-modules-trial-43858.botics.co",
-  api: "https://fe-modules-trial-43858.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "fe-modules-trial-43858",
+  "url": "https://fe-modules-trial-43858.botics.co",
+  "api": "https://fe-modules-trial-43858.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "welcome"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
--- store/femodulestrialAPI/api.js
+++ store/femodulestrialAPI/api.js
@@ -1,58 +1,72 @@
-import axios from "axios"
+import axios from "axios";
 const femodulestrialAPI = axios.create({
   baseURL: "https://fe-modules-trial-43858.botics.co",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function api_docs_schema_retrieve(payload) {
   return femodulestrialAPI.get(`/api-docs/schema/`, {
-    params: { lang: payload.lang }
-  })
+    params: {
+      lang: payload.lang
+    }
+  });
 }
+
 function api_v1_login_create(payload) {
-  return femodulestrialAPI.post(`/api/v1/login/`, payload)
+  return femodulestrialAPI.post(`/api/v1/login/`, payload);
 }
+
 function api_v1_signup_create(payload) {
-  return femodulestrialAPI.post(`/api/v1/signup/`, payload)
+  return femodulestrialAPI.post(`/api/v1/signup/`, payload);
 }
+
 function rest_auth_login_create(payload) {
-  return femodulestrialAPI.post(`/rest-auth/login/`, payload)
+  return femodulestrialAPI.post(`/rest-auth/login/`, payload);
 }
+
 function rest_auth_logout_create(payload) {
-  return femodulestrialAPI.post(`/rest-auth/logout/`)
+  return femodulestrialAPI.post(`/rest-auth/logout/`);
 }
+
 function rest_auth_password_change_create(payload) {
-  return femodulestrialAPI.post(`/rest-auth/password/change/`, payload)
+  return femodulestrialAPI.post(`/rest-auth/password/change/`, payload);
 }
+
 function rest_auth_password_reset_create(payload) {
-  return femodulestrialAPI.post(`/rest-auth/password/reset/`, payload)
+  return femodulestrialAPI.post(`/rest-auth/password/reset/`, payload);
 }
+
 function rest_auth_password_reset_confirm_create(payload) {
-  return femodulestrialAPI.post(`/rest-auth/password/reset/confirm/`, payload)
+  return femodulestrialAPI.post(`/rest-auth/password/reset/confirm/`, payload);
 }
+
 function rest_auth_registration_create(payload) {
-  return femodulestrialAPI.post(`/rest-auth/registration/`, payload)
+  return femodulestrialAPI.post(`/rest-auth/registration/`, payload);
 }
+
 function rest_auth_registration_resend_email_create(payload) {
-  return femodulestrialAPI.post(
-    `/rest-auth/registration/resend-email/`,
-    payload
-  )
+  return femodulestrialAPI.post(`/rest-auth/registration/resend-email/`, payload);
 }
+
 function rest_auth_registration_verify_email_create(payload) {
-  return femodulestrialAPI.post(
-    `/rest-auth/registration/verify-email/`,
-    payload
-  )
+  return femodulestrialAPI.post(`/rest-auth/registration/verify-email/`, payload);
 }
+
 function rest_auth_user_retrieve(payload) {
-  return femodulestrialAPI.get(`/rest-auth/user/`)
+  return femodulestrialAPI.get(`/rest-auth/user/`);
 }
+
 function rest_auth_user_update(payload) {
-  return femodulestrialAPI.put(`/rest-auth/user/`, payload)
+  return femodulestrialAPI.put(`/rest-auth/user/`, payload);
 }
+
 function rest_auth_user_partial_update(payload) {
-  return femodulestrialAPI.patch(`/rest-auth/user/`, payload)
+  return femodulestrialAPI.patch(`/rest-auth/user/`, payload);
 }
+
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
@@ -68,4 +82,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-}
+};
